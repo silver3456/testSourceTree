ad3bdb2328f0c45e5111b6ae1954e820f8c97e4e
silver3456 <avseliverov@gmail.com>
2018-11-15 13:24:09 +0300

    Add 'How's it going?'

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -4,6 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello John!" );
+        System.out.println( "Hello Alex!" );
+        System.out.println( "How's it going?" );
     }
 }
